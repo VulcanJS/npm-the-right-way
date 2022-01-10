b867f2d26d114760bcd057fc1f7a45f55267aebd
eric-burel <eb@lebrun-burel.com>
2022-01-10 15:03:11 +0100

    add esbuild demo, won't build yet

---
+++ demo-next-app/pages/with-esbuild.tsx
@@ -0,0 +1,40 @@
+import { shared, isClient, SharedType } from "my-package-esbuild";
+import { serverOnly, ServerOnlyType } from "my-package-esbuild/server";
+
+// import type { ClientOnlyType } from "my-package-webpack/client";
+import dynamic from "next/dynamic";
+// @ts-ignore
+const DynamicClientOnly = dynamic(
+  () =>
+    import("my-package-esbuild/client").then(
+      (mod) =>
+        function ClientOnlyComponent() {
+          return <>{mod.clientOnly}</>;
+        }
+    ),
+  { ssr: false }
+);
+export default function WithWebpackPage(props: { serverOnly: ServerOnlyType }) {
+  return (
+    <div>
+      <div>Server-only: {props.serverOnly}</div>
+      <div>
+        Client-only: <DynamicClientOnly />
+      </div>
+      <div>Shared:{shared}</div>
+      <p>
+        Note: this line should provoke an SSR issue, it's perfectly normal,
+        that's just for testing
+      </p>
+      <div>Is client: {isClient() ? "true" : "false"} </div>
+    </div>
+  );
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      serverOnly,
+    },
+  };
+}
--- my-package-esbuild/shared/index.ts
+++ my-package-esbuild/shared/index.ts
@@ -1,3 +1,6 @@
+// Import a non-trivial shared lib to test "externals" management
+import "react";
+
 export type SharedType = "shared";
 export const shared: SharedType = "shared";
 
