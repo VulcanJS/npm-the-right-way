ffc157dae687f741915b258d685eedd0e3bfeae2
eric-burel <eb@lebrun-burel.com>
2022-01-14 15:29:41 +0100

    temporary comment out client-only build

--- demo-next-app/pages/with-esbuild.tsx
+++ demo-next-app/pages/with-esbuild.tsx
@@ -3,7 +3,10 @@ import { serverOnly, ServerOnlyType } from "my-package-esbuild/server";
 
 // import type { ClientOnlyType } from "my-package-webpack/client";
 import dynamic from "next/dynamic";
+
 // @ts-ignore
+// FIXME: import() doesn't work with ES modules
+/*
 const DynamicClientOnly = dynamic(
   () =>
     import("my-package-esbuild/client").then(
@@ -13,14 +16,12 @@ const DynamicClientOnly = dynamic(
         }
     ),
   { ssr: false }
-);
+);*/
 export default function WithEsbuildPage(props: { serverOnly: ServerOnlyType }) {
   return (
     <div>
       <div>Server-only: {props.serverOnly}</div>
-      <div>
-        Client-only: <DynamicClientOnly />
-      </div>
+      <div>{/*Client-only: <DynamicClientOnly />*/}</div>
       <div>Shared:{shared}</div>
       <p>
         Note: this line should provoke an SSR issue, it's perfectly normal,
